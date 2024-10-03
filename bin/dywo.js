8142f3a6b4c78ace40ac94167fe304ac557b128d
unknown <bluethefoxoffical@gmail.com>
2024-10-03 03:24:33 +0100

    more stuff

--- bin/dywo.js
+++ bin/dywo.js
@@ -1,66 +1,86 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
-const generate = require('../lib/commands/generate');
-const add = require('../lib/commands/add');
-const compile = require('../lib/commands/compile');
-const serve = require('../lib/commands/serve');
-const test = require('../lib/commands/test');
-const obfuscate = require('../lib/commands/obfuscate');
-const lint = require('../lib/commands/lint');
-const format = require('../lib/commands/format');
+const chalk = require('chalk');
 
 program
   .version('1.0.0')
-  .description('Dywo - A flexible CLI tool for web development');
+  .description('Dywo - Dynamic Web One: A CLI for modern SPA development');
 
 program
-  .command('generate')
-  .description('Generate a new Dywo project with custom options')
-  .action(generate);
+  .command('create <project-name>')
+  .description('Create a new Dywo SPA project')
+  .option('-t, --template <name>', 'Template to use (vanilla, react, vue)', 'vanilla')
+  .option('-p, --pkg <manager>', 'Package manager to use (npm, yarn, pnpm)', 'npm')
+  .action((projectName, options) => {
+    require('../lib/commands/create')(projectName, options);
+  });
 
 program
-  .command('add')
-  .description('Add a new component or page to an existing Dywo project')
-  .action(add);
+  .command('dev')
+  .description('Start development server with hot reloading')
+  .option('-p, --port <number>', 'Port to run the server on', '3000')
+  .action((options) => {
+    require('../lib/commands/dev')(options);
+  });
 
 program
-  .command('compile <target>')
-  .description('Compile the project (client or server)')
-  .option('-m, --mode <mode>', 'Webpack mode (development or production)', 'production')
-  .option('-w, --watch', 'Watch for changes and recompile', false)
-  .option('-a, --analyze', 'Analyze bundle size', false)
-  .action(compile);
+  .command('build')
+  .description('Build the SPA for production')
+  .option('-a, --analyze', 'Analyze the bundle after build')
+  .action((options) => {
+    require('../lib/commands/build')(options);
+  });
 
 program
-  .command('serve')
-  .description('Serve the project')
-  .option('-p, --port <port>', 'Port to serve on', '3000')
-  .action(serve);
+  .command('add <type> <name>')
+  .description('Add a new component, page, or route to the project')
+  .option('-p, --path <directory>', 'Custom path for the new item')
+  .action((type, name, options) => {
+    require('../lib/commands/add')(type, name, options);
+  });
 
 program
-  .command('test <target>')
-  .description('Run tests (client or server)')
-  .option('-w, --watch', 'Watch for changes and rerun tests', false)
-  .option('-c, --coverage', 'Collect test coverage', false)
-  .action(test);
+  .command('test')
+  .description('Run tests')
+  .option('-w, --watch', 'Watch for changes and rerun tests')
+  .option('-c, --coverage', 'Collect test coverage')
+  .action((options) => {
+    require('../lib/commands/test')(options);
+  });
 
 program
-  .command('obfuscate <target>')
-  .description('Obfuscate compiled code (client or server)')
-  .option('-l, --level <level>', 'Obfuscation level (low, medium, high)', 'medium')
-  .action(obfuscate);
+  .command('lint')
+  .description('Lint the project')
+  .option('-f, --fix', 'Automatically fix problems when possible')
+  .action((options) => {
+    require('../lib/commands/lint')(options);
+  });
 
 program
-  .command('lint <target>')
-  .description('Lint code (client or server)')
-  .option('-f, --fix', 'Automatically fix problems', false)
-  .action(lint);
+  .command('optimize')
+  .description('Optimize the SPA bundle')
+  .option('-t, --target <type>', 'Optimization target (size, performance)', 'both')
+  .action((options) => {
+    require('../lib/commands/optimize')(options);
+  });
 
 program
-  .command('format <target>')
-  .description('Format code (client or server)')
-  .option('-w, --write', 'Write formatted code back to files', false)
-  .action(format);
+  .command('repair')
+  .description('Diagnose and repair issues in the Dywo project')
+  .option('-f, --fix', 'Automatically fix identified issues')
+  .action((options) => {
+    require('../lib/commands/repair')(options);
+  });
+
+program
+  .on('command:*', () => {
+    console.error(chalk.red('Invalid command: %s\nSee --help for a list of available commands.'), program.args.join(' '));
+    process.exit(1);
+  });
 
 program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
